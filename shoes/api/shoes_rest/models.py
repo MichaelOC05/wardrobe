a1d41a84cbdf71c73b5c283c6134338f675759c9
Totz96 <lotztim@gmail.com>
2022-07-27 18:57:51 -0400

    created the api for the shoe model

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,14 +1,26 @@
 from django.db import models
-from wardrobe.api.wardrobe_api.models import Bin 
+from django.urls import reverse
+
 
 # Create your models here.
+# class BinVO(models.Model):
+
+
 class Shoes(models.Model):
     manufacturer = models.CharField(max_length=200)
     model_name = models.CharField(max_length=200)
     color = models.CharField(max_length=200)
     picture_url = models.URLField(null=True)
-    bin = models.ForeignKey(
-        Bin,
-        related_name="shoes",
-        on_delete=models.CASCADE,
-    )
+    # bin = models.ForeignKey(
+    #     BinVO,
+    #     related_name="shoes",
+    #     on_delete=models.CASCADE,
+    # )
+
+
+    # puts the href in the 
+    def get_api_url(self):
+        return reverse("api_show_shoe", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.model_name
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+import json
+
+from common.json import ModelEncoder
+from shoes_rest.models import Shoes
 
 # Create your views here.
+class ShoesListEncoder(ModelEncoder):
+    model = Shoes
+    properties = ["model_name", "color"]
+
+
+class ShoesDetailEncoder(ModelEncoder):
+    model = Shoes
+    properties = ["model_name", "color", "picture_url", "manufacturer",]
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoes.objects.all()
+        return JsonResponse(
+                {"shoes": shoes},
+                encoder=ShoesListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        shoe = Shoes.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoesDetailEncoder,
+            safe=False
+        )
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_show_shoe(request, pk):
+    if request.method == "GET":
+        shoe = Shoes.objects.filter(id=pk)
+        return JsonResponse(
+            shoe,
+            encoder=ShoesDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = Shoes.objects.filter(id=pk).delete()
+        return JsonResponse(
+            {"deleted": count > 0}
+        )
+    else: 
+        content = json.loads(request.body)
+        Shoes.objects.filter(id=pk).update(**content)
+        shoe = Shoes.objects.get(id=pk)
+        return JsonResponse(
+            shoe,
+            encoder=ShoesDetailEncoder,
+            safe=False,
+        )
