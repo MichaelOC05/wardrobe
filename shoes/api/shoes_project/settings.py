6d2f6d140bc0843bd393be3e1c8d9ac229de3042
Totz96 <lotztim@gmail.com>
2022-07-27 16:41:19 -0400

    created the Shoes model

--- shoes/api/shoes_project/settings.py
+++ shoes/api/shoes_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "shoes_rest.apps.Shoes_restConfig",
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from wardrobe.api.wardrobe_api.models import Bin 
 
 # Create your models here.
+class Shoes(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True)
+    bin = models.ForeignKey(
+        Bin,
+        related_name="shoes",
+        on_delete=models.CASCADE,
+    )
