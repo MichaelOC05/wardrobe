8c5ca2ef8c870b46bd6ec2104c83ebd9502c8d66
Michael O'Connell <mikematoconnell@gmail.com>
2022-07-28 12:50:09 -0400

    started on hats microservice but have not pulled Tim's shoe microservice yet

--- hats/api/hats_project/settings.py
+++ hats/api/hats_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hats_rest.apps.Hats_restConfig"
     "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from models import Hat, LocationVO
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+import json
+from common.json import ModelEncoder
+
 
 # Create your views here.
+class LocationVOListEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["closet_name", "section_number", "shelf_number", "import_href"]
+
+class HatsListEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "style_name",
+        "location"      
+    ]
+
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "style_name",
+        "color",
+        "picture_url",
+        "fabric"
+    ]
+    encoders = {"location": LocationVOListEncoder()}
+
+@require_http_methods(["GET", "POST"])
+def api_list_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse(
+            hats,
+            encoder=HatsListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            location = LocationVO.objects.get(import_href=content["location"])
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return (JsonResponse
+                {"message": "Invalid location href"},
+                status=400
+            )
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatDetailEncoder,
+            safe=False
+        )
