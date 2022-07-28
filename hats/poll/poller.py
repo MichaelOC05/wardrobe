e8ebc417af58c654526866e89e7acf62e0bbed71
Totz96 <lotztim@gmail.com>
2022-07-28 14:25:18 -0400

    changed the pollers sleep time to 60

--- hats/poll/poller.py
+++ hats/poll/poller.py
@@ -35,7 +35,7 @@ def poll():
             get_location()
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(5)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
